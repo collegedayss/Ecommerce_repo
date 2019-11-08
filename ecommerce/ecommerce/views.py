712297ef6dc352dcf1b480d2e0bfa09d326ec6c1
Moiz <HustlersParadise@MacBook-Pro.fios-router.home>
2019-11-07 21:38:42 -0500

    02 | 013

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -3,4 +3,21 @@ from django.shortcuts import render
 
 
 def home_page(request):
+    context = {
+        'title': "Home Page"
+    }
+    return render(request, "home_page.html", context)
+
+
+def about_page(request):
+        context = {
+        'title': "ABOUT Page"
+    }
+    return render(request, "home_page.html", {})
+
+
+def contact_page(request):
+        context = {
+        'title': "CONTACT Page"
+    }
     return render(request, "home_page.html", {})
