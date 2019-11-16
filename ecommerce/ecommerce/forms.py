c04ff0a1430f451b20a82ffd499433913d1476cc
Muhammad Moiz Saeed <moizsaeed10@gmail.com>
2019-11-16 11:16:52 -0500

    02 || 15 Django Forms.

---
+++ ecommerce/ecommerce/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    fullname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                'id': 'form_full_name'
+            })
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control"
+            }))
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+            })
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not 'gmail.com' in email:
+            raise forms.ValidationError('Email has to be Gmail')
+        return email
--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def home_page(request):
@@ -17,10 +18,17 @@ def about_page(request):
 
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+
     context = {
-        'title': "CONTACT Page"
+        'title': "CONTACT Page",
+        'form': contact_form
     }
-    if request.method == "POST":
-        print(request.POST)
-        print(request.POST.get('fullname'))
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
