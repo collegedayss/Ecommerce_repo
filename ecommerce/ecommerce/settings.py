2ed6d38913419e42ef4e894cde759684c2833336
Muhammad Moiz Saeed <moizsaeed10@gmail.com>
2019-11-16 14:34:33 -0500

    03 || 22  'Project Model'

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    # our Apps
+    'products',
+    # preinstalled Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
