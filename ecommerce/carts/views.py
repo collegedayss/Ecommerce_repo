4c871e1f9aabcba8d2560881a873db89966b64c2
Muhammad Moiz Saeed <moizsaeed10@gmail.com>
2019-12-05 23:21:03 -0500

    Creating Model classes for the Cart Application.

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 
 
 def cart_home(request):
+    cart_id = request.session.get("cart_id", None)
+
+    if cart_id is None:  # and isinstance('cart_id', None):
+        print("create new cart")
+        request.session['cart_id'] = 12
+    else:
+        print('Cart id exists')
+   # print(request.session.session_key, request.session)  #
     return render(request, "carts/home.html", {})
